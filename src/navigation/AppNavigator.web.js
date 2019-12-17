f64d1e1964e7d800072aa9c10623f7f24887a3d0
victors1681 <v.santos@itsoluclick.com>
2019-12-16 20:22:53 -0500

    fixing eslint

--- src/navigation/AppNavigator.web.js
+++ src/navigation/AppNavigator.web.js
@@ -3,14 +3,14 @@ import {createStackNavigator} from 'react-navigation-stack';
 import {createSwitchNavigator} from 'react-navigation';
 import MainTabNavigator from './MainTabNavigator';
 import SignInScreen from '../screens/SignInScreen';
-import AuthLoadingScreen from '../screens/AuthLoadingScreen';
+// import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 
 // const AppStack = createStackNavigator({ Home: MainTabNavigator });
 const AuthStack = createStackNavigator({SignInScreen});
 
 const switchNavigator = createSwitchNavigator(
   {
-    AuthLoading: AuthLoadingScreen,
+    // AuthLoading: AuthLoadingScreen,
     App: MainTabNavigator,
     Auth: AuthStack,
   },
