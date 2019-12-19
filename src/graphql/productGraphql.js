47ca429e9fc98cffa6c9717cf3a91e321b375767
victors1681 <v.santos@itsoluclick.com>
2019-12-19 15:52:46 -0500

    refactoring dirs

---
+++ src/graphql/productGraphql.js
@@ -0,0 +1,50 @@
+import gql from 'graphql-tag';
+
+export const GET_PRODUCTS = gql`
+  query PRODUCTS($description: String) {
+    products(description: $description, limit: 10) {
+      code
+      name
+      description
+      status
+      price {
+        name
+        price
+        type
+        value
+      }
+      tax {
+        id
+        name
+        percentage
+        deductible
+        description
+        status
+      }
+      category {
+        name
+        description
+      }
+      inventory {
+        unit {
+          shortName
+        }
+        unit {
+          name
+        }
+        initialQuantity
+      }
+    }
+  }
+`;
+
+export const ADD_ITEM = gql`
+  mutation Add_Item($item: String) {
+    addItem(item: $item) @client
+  }
+`;
+export const REMOVE_ITEM = gql`
+  mutation Remove_Item($itemId: String) {
+    removeItem(itemId: $itemId) @client
+  }
+`;
---
+++ src/resolvers/document/document.js
@@ -0,0 +1,65 @@
+import {GET_CURRENT_DOCUMENT} from '../../graphql/documentGraphql';
+
+const updateTotals = items => {
+  let totalTax = 0;
+
+  const total = items.reduce((acc, current) => {
+    const currentTax = current.tax
+      .map(tax => (tax.percentage / 100 + 1) * current.price - current.price)
+      .reduce((a, c) => a + c);
+    totalTax += currentTax * current.quantity;
+    return acc + (current.price + currentTax) * current.quantity;
+  }, 0);
+
+  return {total, totalTax};
+};
+
+const documentResolvers = {
+  Mutation: {
+    addItem: (_root, {item}, {cache, getCacheKey}) => {
+      const id = getCacheKey({__typename: 'documentCreation'});
+
+      const {document} = cache.readQuery({
+        query: GET_CURRENT_DOCUMENT,
+      });
+
+      const {total, totalTax} = updateTotals([...document.items, item]);
+      const doc = {
+        ...document,
+        items: [...document.items, item],
+        total,
+        totalTax,
+      };
+      console.log('ADDING NEW ITEM!!!!', doc, item);
+
+      cache.writeData({
+        data: {
+          document: doc,
+        },
+      });
+
+      return doc.items;
+    },
+    removeItem: (_root, {itemId}, {cache, getCacheKey}) => {
+      const {document} = cache.readQuery({
+        query: GET_CURRENT_DOCUMENT,
+      });
+
+      console.log('removing', itemId);
+
+      const items = document.items.filter(item => item.code !== itemId);
+
+      const {total, totalTax} = updateTotals(items);
+
+      cache.writeData({
+        data: {
+          document: {...document, items, total, totalTax},
+        },
+      });
+
+      return document;
+    },
+  },
+};
+
+export default documentResolvers;
